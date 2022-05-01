7cf5e1ede77a054181e70e2c0fd848b4f793f107
Jasmine Callon <jascallon99@gmail.com>
2022-05-01 10:21:23 -0700

    bug fixes galore, positive case works for each command

--- main.py
+++ main.py
@@ -9,36 +9,22 @@ import re
 #from io import BytesIO
 import file_list
 
-# Define bot, see discordpy website for logging to file instead of stdout
+# Define logging
 logging.basicConfig(level=logging.INFO)
-
-#bot = discord.bot()
-
-# Define behavior on bot login
-#@bot.event
-#async def on_ready():
-#    print("Logged on as {0.user}!".format(bot))
-
-# Define behavior on bot message receival
-#@bot.event
-#async def on_message(message):
-#    if message.author == bot.user:
-#        return
-#    print("Message from {0.author}: {0.content}".format(message))
+# Define bot
+bot = commands.Bot(command_prefix="$")
 
 #=================#
 # Define commands #
 #=================#
-# I plan to only support once voice_client at a time
-bot = commands.Bot(command_prefix="$")
+# Note: I plan to only support once voice_client at a time
 #------#
 # join #
 #------#
 @bot.command()
 async def join(ctx):
-    author = ctx.author
     # Author must have a voice connection to join
-    if type(author) != discord.Member or author.voice == None:
+    if type(ctx.author) != discord.Member or ctx.author.voice == None:
         await ctx.send("You must be in a guild voice chat and use \join from a guild text chat.")
         return
     # If in a voice channel already, leave it
@@ -47,7 +33,7 @@ async def join(ctx):
 
     voice_channel = ctx.author.voice.channel
     voice_client = await voice_channel.connect()
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("bark.mp3"))
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("./bark.mp3"))
     voice_client.play(source)
 
 #-------#
@@ -59,9 +45,10 @@ async def leave(ctx):
         await ctx.send("I'm already not in a voice channel.")
         return
 
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("bark.mp3"))
-    bot.voice_voice_clients[0].play(source, after=lambda e:
-        await bot.voice_clients[0].disconnect())
+    # TODO play exit sound, not sure how to wait for sound to finish to disconnect
+    #source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("./bark.mp3"))
+    #bot.voice_clients[0].play(source, after=lambda e:
+    await bot.voice_clients[0].disconnect()#)
 
 #------#
 # stop #
@@ -79,7 +66,7 @@ async def stop(ctx):
 #------#
 #@bot.command()
 #async def play(ctx, url):
-#    #match = re.search("\Ahttps://(www.youtube.com/watch?v=)|(youtu.be/)[a-zA-Z0-9]+", url)
+#    #match = re.search(r"\Ahttps://(www.youtube.com/watch?v=)|(youtu.be/)[a-zA-Z0-9]+", url)
 #    #if match == None:
 #        #await ctx.send("I don't understand the format of that link. Did you copy the full link?")
 #
@@ -94,9 +81,9 @@ async def stop(ctx):
 #    player = await YTDLSource.from_url(url, stream=True)
 #    voice_client.play(player)
 
-#------#
-# tts  #
-#------#
+#-----#
+# tts #
+#-----#
 #@bot.command()
 #async def tts(ctx, *, arg):
 #    if voice_client == None:
@@ -113,6 +100,7 @@ async def stop(ctx):
 #------#
 @bot.command()
 async def save(ctx):
+    # Warn the user if they forgot an attachment
     if len(ctx.message.attachments) == 0:
         await ctx.send("You didn't give me an attachment to save.")
         return
@@ -120,23 +108,24 @@ async def save(ctx):
     for attachment in ctx.message.attachments:
         await ctx.send("Processing \"{}\"...".format(attachment.filename))
         await file_list.save_audio_file(ctx, attachment)
-    await ctx.send("Done.")
 
 #------#
 # load #
 #------#
 @bot.command()
 async def load(ctx, name):
+    mp3_name = file_list.get_mp3_name(name)
     # Don't open a file that doesn't exist
-    if name not in file_list.file_cache:
+    if mp3_name == None:
         await ctx.send("Could not find \"{}.mp3\".".format(name))
         return
+    # Tell user if there's no voice client to play into anyways
     if len(bot.voice_clients) == 0:
         await ctx.send("I have no voice chat to load into.")
         return
 
-    await ctx.send("Will now play \"{}.mp3\".".format(name))
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("./audio/{}.mp3".format(name)))
+    await ctx.send("Will now play \"{}\".".format(mp3_name))
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("./audio/{}".format(mp3_name)))
     bot.voice_clients[0].play(source)
 
 #--------#
@@ -145,6 +134,32 @@ async def load(ctx, name):
 @bot.command()
 async def delete(ctx, name):
     await file_list.delete_audio_file(ctx, name)
+
+#------#
+# list #
+#------#
+@bot.command()
+async def list(ctx):
+    full_message = "```\n"
+    for i in range(len(file_list.file_cache)):
+        full_message += str(i) + ". " + file_list.file_cache[i] + "\n"
+    full_message += "```"
+
+    await ctx.send(full_message)
+
+#-----#
+# get #
+#-----#
+@bot.command()
+async def get(ctx, name):
+    mp3_name = file_list.get_mp3_name(name)
+    # Don't get a file that doesn't exist
+    if mp3_name == None:
+        await ctx.send("Could not find \"{}.mp3\".".format(name))
+        return
+
+    file_handle = open("./audio/{}".format(mp3_name), "rb")
+    await ctx.send(file=discord.File(file_handle))
 
 # Log in
 bot.run(json.load(open("auth/discord.json", "r"))["token"])
